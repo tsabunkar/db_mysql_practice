7078c389928bd379929c68164a860da4b2a9bcd1
Tejas Sabunkar <tsabunkar@gmail.com>
2019-11-17 15:32:37 +0530

    challenges 3

--- case_study/instagram/challenges.sql
+++ case_study/instagram/challenges.sql
@@ -73,3 +73,98 @@ SELECT username
 FROM users u LEFT JOIN photos p
     ON u.uid = p.user_id
 WHERE p.pid IS NULL;
+
+----------------------------
+
+/*
+Problem 4:
+We're running a new contest to see who can get the most likes on a single photo.
+WHO WON ??
+*/
+
+SELECT *
+FROM likes;
+
+-- I need to know user who has got most likes on a single photo
+
+
+-- subtask-1: find most number of likes on a single photo
+
+SELECT *, COUNT(*)
+FROM likes
+GROUP BY photo_id;
+
+SELECT *, COUNT(*)
+FROM likes
+GROUP BY photo_id;
+
+
+SELECT *, COUNT(*) count
+FROM likes
+GROUP BY photo_id
+ORDER BY count DESC;
+
+-- !NOTE : The above userId is not the actual user id which has maximum number of likes for its photo
+-- !bcoz : when we run below query, we have 48 uid for this pid = 145
+
+SELECT *
+FROM likes
+WHERE photo_id = 145;
+
+-- NOW we need to find which uid has max no of likes in pid=145
+
+SELECT *
+FROM photos
+WHERE pid =145;
+
+-- from above query we know that its uid is 52;
+
+SELECT *
+FROM users
+WHERE uid = 52;
+
+/* 
+
+SELECT *, COUNT(*) count
+FROM likes
+GROUP BY photo_id
+ORDER BY count DESC
+LIMIT 1;
+
+*/
+
+
+------Next try
+
+
+SELECT *, COUNT(*)
+FROM likes l INNER JOIN photos p
+    ON l.photo_id = p.pid
+GROUP BY pid;
+
+SELECT *, COUNT(*)
+FROM likes l INNER JOIN photos p
+    ON l.photo_id = p.pid
+GROUP BY pid
+ORDER BY COUNT(*) DESC;
+
+SELECT *, COUNT(*)
+FROM likes l INNER JOIN photos p
+    ON l.photo_id = p.pid
+    INNER JOIN users u
+    ON p.user_id = u.uid
+GROUP BY pid
+ORDER BY COUNT(*) DESC;
+
+/* 
+
+SELECT username
+FROM likes l INNER JOIN photos p
+    ON l.photo_id = p.pid
+    INNER JOIN users u
+    ON p.user_id = u.uid
+GROUP BY pid
+ORDER BY COUNT(*) DESC
+LIMIT 1; 
+
+*/
